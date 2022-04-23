437b46f5d816235dd9f40329e077effbe4d855e1
Madi Combe <54414117+MadiCombe@users.noreply.github.com>
2022-04-22 22:34:00 -0500

    citation

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -1,5 +1,5 @@
 extends RigidBody2D
-
+#https://docs.godotengine.org/en/3.1/tutorials/2d/2d_movement.html?highlight=click%20and%20move#click-and-move
 export (int) var speed = 100
 
 var target = Vector2()
@@ -11,6 +11,5 @@ func _input(event):
 
 func _physics_process(delta):
 	velocity = (target - position).normalized() * speed
-
 	if (target - position).length() > 5:
 		velocity = move_and_slide(velocity)
