14707769c2635a1194a3b4b1dbb917f45a54235d
Madi Combe <54414117+MadiCombe@users.noreply.github.com>
2022-04-22 20:17:10 -0500

    barf

--- Player.gd
+++ Player.gd
@@ -1,16 +1,44 @@
 extends Node2D
 
+onready var sprite = $Sprite
+export var player = 0;
+onready var tween = $Tween
+var sprites = [preload("res://Assets/queen_b.png"), preload("res://Assets/baby_b.png"), preload("res://Assets/worker_b.png"), preload("res://Assets/drone_b.png")]
+var space = 0
+var dir = Vector2.RIGHT
+var speed = 2
+#var tilesize = 128
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal movedone
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	sprite.texture = sprites[player]
+	position.x = 64
+	position.y = 64
 
+func move(spaces):
+	for n in spaces:
+		movespace()
+		yield(tween, "tween_completed")
+		#dont have this (yet?) vvv
+		#GameState.update_spaceLabel(space)
+	emit_signal("movedone")
 
+func movespace():
+	match space:
+		0: dir = Vector2.RIGHT
+		11: dir = Vector2.DOWN
+		18: dir = Vector2.LEFT
+		29: dir = Vector2.UP
+	space = (space + 1) % 36
+	#tween.interpolate_property(self, "position", position,
+	#position + dir * tilesize, 1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
+	
+		
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
